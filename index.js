7b9d8f820dc2917ecc22c89dcc03893a079c4f22
its-kayes <kayes.ek8@gmail.com>
2022-05-13 03:57:14 +0600

    Load Data form db

--- index.js
+++ index.js
@@ -8,18 +8,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = "mongodb+srv://doctor_admin:<password>@cluster0.af5hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://doctor_admin:MkoOVty4ROTRMPfz@cluster0.af5hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.af5hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.af5hd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        let doctorServicesdb = client.db("doctor_portal").collection("services");
 
 
+        app.get('/services', async(req, res)=> {
+            let query = {}
+            let cursor = doctorServicesdb.find(query);
+            let result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }
+
+    finally {
+        
+    }
+
+
+
+}
+
+
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Doctor On Fire!')
